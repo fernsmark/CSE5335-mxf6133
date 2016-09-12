8f0cc47a390168bfb061ace41bddc36758f455de
fernsmark <ferns.mark@gmail.com>
2016-09-12 15:57:17 -0500

    MF09122016- Favicon removed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //https://www.npmjs.com/package/serve-favicon
-app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use('/', routes);
 app.use('/users', users);

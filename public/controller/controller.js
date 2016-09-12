ff72162de2ca0c39365442eb9ed5765c14b5f019
fernsmark <ferns.mark@gmail.com>
2016-09-12 14:40:42 -0500

    MF09122016- http: dropped from $http.get()

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,5 +1,6 @@
 /**
  * Created by Mark on 9/11/2016.
+ * Note on http.get(): 'http:' is dropped from API request to make this request relative to the current https protocol used by Heroku
  */
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http',
@@ -11,7 +12,7 @@ myApp.controller('AppCtrl', ['$scope', '$http',
         };
         
         function fetch(){
-            $http.get("http://api.geonames.org/findNearbyPostalCodesJSON?postalcode="+$scope.getCode+"&country=US&radius=6&maxRows=5&username=ferns.mark")
+            $http.get("//api.geonames.org/findNearbyPostalCodesJSON?postalcode="+$scope.getCode+"&country=US&radius=6&maxRows=5&username=ferns.mark")
                 .then(function(response) {
                     $scope.postalCodes = response.data.postalCodes;
                     console.log($scope.postalCodes);
